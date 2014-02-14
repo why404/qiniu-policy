91d3ca299c5a9e9290d0905b3e88f0420d52c168
why404 <awhy.xu@gmail.com>
2014-02-14 23:44:51 +0800

    fix test specs

--- spec/qiniu/policy/put_spec.rb
+++ spec/qiniu/policy/put_spec.rb
@@ -4,14 +4,19 @@ describe Qiniu::Policy::Put do
 
   context '.token' do
     it 'should works' do
-      expect_uptoken = 'access_key:kyLwxo3HWXBn7ZY884IpS_DVjuM=:eyJkZWFkbGluZSI6MCwic2NvcGUiOiJteV92aWRlb19idWNrZXQifQ=='
+      # cuz json string no sort
+      expected_result = [
+        'access_key:kyLwxo3HWXBn7ZY884IpS_DVjuM=:eyJkZWFkbGluZSI6MCwic2NvcGUiOiJteV92aWRlb19idWNrZXQifQ==',
+        'access_key:wyz1S9nF7Sg-KWyQqn6UxjSr4kY=:eyJzY29wZSI6Im15X3ZpZGVvX2J1Y2tldCIsImRlYWRsaW5lIjowfQ=='
+      ]
       mac = Qiniu::Kit::Mac.new('access_key', 'secret_key')
       put_policy = Qiniu::Policy::Put.new(mac) do |f|
         # let ruby 1.8.x and ruby 1.9.x both follow the in insertion order
         f.deadline = 0
         f.scope = 'my_video_bucket'
       end
-      put_policy.token.should eq(expect_uptoken)
+      result = put_policy.token
+      expected_result.include?(result).should be_true
     end
   end
 
