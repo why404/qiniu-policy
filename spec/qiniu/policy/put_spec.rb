d83b948b969b9811533dccc741321d08404bceeb
why404 <awhy.xu@gmail.com>
2014-02-14 22:21:49 +0800

    in insertion order

--- spec/qiniu/policy/put_spec.rb
+++ spec/qiniu/policy/put_spec.rb
@@ -4,11 +4,12 @@ describe Qiniu::Policy::Put do
 
   context '.token' do
     it 'should works' do
-      expect_uptoken = 'access_key:wyz1S9nF7Sg-KWyQqn6UxjSr4kY=:eyJzY29wZSI6Im15X3ZpZGVvX2J1Y2tldCIsImRlYWRsaW5lIjowfQ=='
+      expect_uptoken = 'access_key:kyLwxo3HWXBn7ZY884IpS_DVjuM=:eyJkZWFkbGluZSI6MCwic2NvcGUiOiJteV92aWRlb19idWNrZXQifQ=='
       mac = Qiniu::Kit::Mac.new('access_key', 'secret_key')
       put_policy = Qiniu::Policy::Put.new(mac) do |f|
+        # let ruby 1.8.x and ruby 1.9.x both follow the in insertion order
+        f.deadline = 0
         f.scope = 'my_video_bucket'
-        f.deadline = 0 # Time.now.to_i + 3600
       end
       put_policy.token.should eq(expect_uptoken)
     end
